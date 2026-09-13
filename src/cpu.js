a50ebad0220bdb0e5082a54efe9068b27e481c3e
SaadDev <zwixozouine@gmail.com>
2026-09-13 22:57:29 +0100

    finding the percentage

--- src/cpu.js
+++ src/cpu.js
@@ -3,23 +3,42 @@ import os, { type } from "os";
 let CPU_Model = null;
 let CPU_SPEED = null;
 
+
+
 // tracking the CPU usage 
-function CPU_Usage(){
+function CPU_Usage() {
     const cpu_infos = os.cpus();
     // check if is not array or i the length of it is empty : if true no cpu exist
-    if(!Array.isArray(cpu_infos) || cpu_infos.length === 0) return "Cpu Not found";
-
+    if (!Array.isArray(cpu_infos) || cpu_infos.length === 0) return "Cpu Not found";
 
     // if that went correct
     let totatTime = 0;
     let totalIdle = 0;
 
     cpu_infos.forEach((core) => {
-        for (const type in  core.times){
+        for (const type in core.times) {
             totatTime += core.times[type];
         }
         totalIdle += core.times.idle;
     });
 
-    return {totatTime, totalIdle};
+    return { totatTime, totalIdle };
 }
+
+
+const startTrack = CPU_Usage()
+
+setInterval(() => {
+    const endTrack = CPU_Usage();
+
+    // console.log(endTrack.totalIdle - startTrack.totalIdle);
+    // console.log(endTrack.totatTime - startTrack.totatTime);
+    const totalIdleDiff = endTrack.totalIdle - startTrack.totalIdle;
+    const totalTimeDiff = endTrack.totatTime - startTrack.totatTime;
+
+    console.log(100 - Math.floor((100 * totalIdleDiff) / totalTimeDiff)); 
+
+}, 1000);
+
+
+
