3461667324b9fbd1a5b64a727c09480eaf8404fb
SaadDev <zwixozouine@gmail.com>
2026-09-13 23:50:14 +0100

    all is working for now

---
+++ src/display/trying.js
@@ -0,0 +1,12 @@
+import {getFree_Memory, getTotal_memo} from '../core/memory.js';
+import {CPU_Model, CPU_SPEED} from "../core/cpu.js"
+
+console.log(getFree_Memory());
+console.log(getTotal_memo());
+
+console.log(CPU_Model);
+console.log(CPU_SPEED, "MHz");
+
+
+
+
