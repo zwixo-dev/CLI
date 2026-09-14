8e3fd00c5986d6293b74552eff0d337625dd7593
SaadDev <zwixozouine@gmail.com>
2026-09-14 23:51:09 +0100

    hmm something went wrong here let try another test

--- src/display/trying.js
+++ src/display/trying.js
@@ -1,12 +1,19 @@
 import {getFree_Memory, getTotal_memo} from '../core/memory.js';
-import {CPU_Model, CPU_SPEED} from "../core/cpu.js"
+// import {CPU_Model, CPU_SPEED} from "../core/cpu.js"
+import {getNetworkUploadSpeed} from "../core/network.js";
 
-console.log(getFree_Memory());
-console.log(getTotal_memo());
+// console.log(CPU_Model);
+// console.log(CPU_SPEED, "MHz");
+console.log("current free memory :",getFree_Memory());
+console.log("you total memory is : ",getTotal_memo());
 
-console.log(CPU_Model);
-console.log(CPU_SPEED, "MHz");
 
 
+
+
+setInterval(async() => {
+    const speed_test = await getNetworkUploadSpeed();
+    console.log(speed_test);
+}, 1000);
 
 
