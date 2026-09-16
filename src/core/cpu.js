1585cc23ac908d0417c40fe7f6d0dbfabf7d95d2
SaadDev <zwixozouine@gmail.com>
2026-09-16 22:33:54 +0100

    corr CPU_Usage func

--- src/core/cpu.js
+++ src/core/cpu.js
@@ -3,10 +3,6 @@ import os, { type } from "os";
 let CPU_Model = os.cpus()[0].model;
 let CPU_SPEED = os.cpus()[0].speed;
 
-// console.log(CPU_Model);
-// console.log(CPU_SPEED);
-
-
 // tracking the CPU usage 
 function CPU_Usage() {
     const cpu_infos = os.cpus();
@@ -14,17 +10,17 @@ function CPU_Usage() {
     if (!Array.isArray(cpu_infos) || cpu_infos.length === 0) return "Cpu Not found";
 
     // if that went correct
-    let totatTime = 0;
+    let totalTime = 0;
     let totalIdle = 0;
 
     cpu_infos.forEach((core) => {
         for (const type in core.times) {
-            totatTime += core.times[type];
+            totalTime += core.times[type];
         }
         totalIdle += core.times.idle;
     });
 
-    return { totatTime, totalIdle };
+    return { totalTime, totalIdle };
 }
 
 export {CPU_Model, CPU_SPEED, CPU_Usage};
@@ -35,9 +31,9 @@ export {CPU_Model, CPU_SPEED, CPU_Usage};
 //     const endTrack = CPU_Usage();
 
 //     // console.log(endTrack.totalIdle - startTrack.totalIdle);
-//     // console.log(endTrack.totatTime - startTrack.totatTime);
+//     // console.log(endTrack.totalTime - startTrack.totalTime);
 //     const totalIdleDiff = endTrack.totalIdle - startTrack.totalIdle;
-//     const totalTimeDiff = endTrack.totatTime - startTrack.totatTime;
+//     const totalTimeDiff = endTrack.totalTime - startTrack.totalTime;
 
 //     console.log(100 - Math.floor((100 * totalIdleDiff) / totalTimeDiff)); 
 
