5e5c855b69bf1106b2adc0f0525e8f4e4230f7e2
SaadDev <zwixozouine@gmail.com>
2026-09-16 22:34:57 +0100

    corr cpuTracking func and display disck infos

--- src/display/dashboard.js
+++ src/display/dashboard.js
@@ -1,17 +1,20 @@
 import { CPU_Model, CPU_SPEED, CPU_Usage } from "../core/cpu.js";
 import { getFree_Memory, getTotal_memo } from "../core/memory.js";
 import { getNetworkUploadSpeed } from "../core/network.js";
+import { diskSpace } from "../core/disk.js";
 import ansiColors from "ansi-colors";
 
 // ---- function for cpu tracking ----
-const startTrack = CPU_Usage();
+let startTrack = CPU_Usage();
+
 function cpuTracking() {
   const endTrack = CPU_Usage();
 
   // calculating the diff
   const totalIdleDiff = endTrack.totalIdle - startTrack.totalIdle;
-  const totalTimeDiff = endTrack.totatTime - startTrack.totatTime;
-
+  const totalTimeDiff = endTrack.totalTime - startTrack.totalTime;
+  if (totalTimeDiff === 0) return 0;
+  startTrack = endTrack;
   return 100 - Math.floor((100 * totalIdleDiff) / totalTimeDiff);
 }
 
@@ -26,10 +29,11 @@ setInterval(async() => {
     process.stdout.write(`\x1b[K${ansiColors.bold("CPU Speed:")} ${ansiColors.green(`${CPU_SPEED} MHz`)}\n`);
     process.stdout.write(`\x1b[K${ansiColors.bold("CPU Usage:")} ${ansiColors.green(`${cpuTracking()}%`)}\n`);
     process.stdout.write(`\x1b[K${ansiColors.bold("RAM:")} ${ansiColors.green(`${getTotal_memo()}/${getFree_Memory().toFixed(4)} GB`)}\n`);
+    process.stdout.write(`\x1b[K${ansiColors.bold("Disk:")} T ${ansiColors.green(`${diskSpace.totalSpace} GB`)} | F ${ansiColors.green(`${diskSpace.freeSpace} GB`)} | U ${ansiColors.green(`${diskSpace.usedSpace} GB`)}\n`);
 
     // network infos 
     if(networkSpeedTest && Object.keys(networkSpeedTest).length === 3){
-        process.stdout.write(`\x1b[K${ansiColors.bold("Network:")} ${ansiColors.cyan(`${networkSpeedTest.bps}bps | ${networkSpeedTest.kbps}kbps | ${networkSpeedTest.kbps}mbps`)}\n`);
+        process.stdout.write(`\x1b[K${ansiColors.bold("Network:")} ${ansiColors.cyan(`${networkSpeedTest.bps}bps | ${networkSpeedTest.kbps}kbps | ${networkSpeedTest.mbps}mbps`)}\n`);
     }else{
         process.stdout.write(`\x1b[K${ansiColors.bold("Network:")} ${ansiColors.red("loading...!")}\n`);
     }
