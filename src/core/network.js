077972814ef5d92a32df7dfe3cdca4c38eb8a6b6
SaadDev <zwixozouine@gmail.com>
2026-09-18 00:01:31 +0100

    i need to corr network file

--- src/core/network.js
+++ src/core/network.js
@@ -7,7 +7,6 @@ async function getNetworkDownloadSpeed() {
     const baseUrl = 'https://eu.httpbin.org/stream-bytes/500000';
     const fileSizeInBytes = 500000;
     const speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
-    console.log(speed);
 }
 
 
